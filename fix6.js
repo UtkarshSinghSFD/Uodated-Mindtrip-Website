4864af79feee2c1b1419addb6d223301fd8cb2a8
UtkarshSInGh1122 <utkarshsolanki69@gamil.com>
2026-08-27 10:47:14 +0530

    chore: remove temporary scripts and add images

--- fix6.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-// Replace image for 12 (FlatForge AI)
-html = html.replace(/(<img[^>]*src="st images\/)[^"]+("[^>]*>\s*<div[^>]*>.*<\/div>\s*<h3>12 &mdash; FlatForge AI<\/h3>)/g, '$1flatforge_new.jpg$2');
-
-fs.writeFileSync('index.html', html);
